6145c8f55eac132d706a9976a6e76e2c3d05f71d
guiireal <guii_real@hotmail.com>
2024-12-24 15:47:02 -0300

    wip

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Página home</h1>
+    <div className="flex flex-col items-center justify-center w-full py-4">
+      <h1 className="mt-20 text-3xl font-bold text-white md:text-4xl">
+        Sujeito programador
+      </h1>
+      <span className="mt-3 mb-5 text-gray-50">Veja meus links 👇</span>
+
+      <main>
+        <section>
+          <a href="#">
+            <p>Canal no YouTube</p>
+          </a>
+        </section>
+      </main>
     </div>
   );
 }
