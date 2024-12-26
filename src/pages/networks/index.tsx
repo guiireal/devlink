c4164e352201383ea8a88b155080db4ee2a33faf
guiireal <guii_real@hotmail.com>
2024-12-26 01:42:45 -0300

    wip

--- src/pages/networks/index.tsx
+++ src/pages/networks/index.tsx
@@ -1,5 +1,5 @@
-import { doc, setDoc } from "firebase/firestore";
-import { FormEvent, useState } from "react";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { FormEvent, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { db } from "../../services/firebaseConnection";
@@ -19,6 +19,23 @@ export default function Networks() {
     });
   }
 
+  useEffect(() => {
+    async function getLinks() {
+      const docRef = doc(db, "social", "link");
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const data = docSnap.data();
+
+        setFacebook(data?.facebook);
+        setInstagram(data?.instagram);
+        setYoutube(data?.youtube);
+      }
+    }
+
+    getLinks();
+  }, []);
+
   return (
     <div className="flex flex-col items-center min-h-screen px-2 pb-7">
       <Header />
@@ -32,18 +49,21 @@ export default function Networks() {
         <Input
           placeholder="Digite a URL do Facebook..."
           value={facebook}
+          type="url"
           onChange={(event) => setFacebook(event.target.value)}
         />
         <label className="my-2 font-medium text-white">Link do Instagram</label>
         <Input
           placeholder="Digite a URL do Instagram..."
           value={instagram}
+          type="url"
           onChange={(event) => setInstagram(event.target.value)}
         />
         <label className="my-2 font-medium text-white">Link do YouTube</label>
         <Input
           placeholder="Digite a URL do YouTube..."
           value={youtube}
+          type="url"
           onChange={(event) => setYoutube(event.target.value)}
         />
         <button
